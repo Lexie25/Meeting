3562be75f235626534556fd2c6550ff6b70735e8
Brenda alexia <brendinha.ae@gmail.com>
2020-03-10 12:29:50 -0300

    corrigido mapeamento controllers

--- src/main/java/com/br/Meeting/Controller/RoomController.java
+++ src/main/java/com/br/Meeting/Controller/RoomController.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.br.Meeting.Service.RoomService;
 import com.br.Meeting.model.Room;
 
 @RestController
+@RequestMapping("room")
 public class RoomController {
 
 	@Autowired
